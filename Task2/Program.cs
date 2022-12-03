8a93676a372e1487782de23dc40c48ce5e23ea63
Denis Krinitsyn <skylinekrin@gmail.com>
2022-12-03 11:09:25 +0500

    Task2 done

---
+++ Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num/100 == 0)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine(num%10);
+}
